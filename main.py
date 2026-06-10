ac242d1781509aaaae70877704a613cce3edafa2
WMFong0 <wmfongyt@gmail.com>
2026-06-10 15:43:42 +0800

    Fix workflow naming haha. Sorry Jimmy

--- main.py
+++ main.py
@@ -814,7 +814,7 @@ def ocr_url(req: UrlOCRRequest) -> dict[str, str]:
         request_headers = {
             # Browser Impersonation
             "User-Agent": (
-                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)"
+                """Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36"""
             ),
             "Accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
             "Accept-Language": "en-US,en;q=0.9",
